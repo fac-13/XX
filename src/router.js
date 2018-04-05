585c5e1a7b718722dccfeb04e85f90050ee62176
tspeed90 <tspeed90@gmail.com>
2018-04-05 14:23:52 +0100

    fixes router export using curly braces

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const router = (request, response) => {
   const url = request.url;
 
   if (url === '/') {
+    console.log(url);
     staticHandler(response, 'public/index.html');
   } else if (url.indexOf('public') !== -1) {
     staticHandler(response, url);
