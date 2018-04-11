a15ddae3c30f3819004c84408f3891cb328dd702
Parissai <parissa.jamali@gmail.com>
2018-04-11 09:58:16 +0100

    refactor addReview Relates #4

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -7,30 +7,27 @@ const addPlace = (name, description, cb) => {
     (err, res) => {
       if (err) {
         return cb(err);
-      } else {
-        cb(null, res);
       }
-    });
+      return cb(null, res);
+    },
+  );
 };
 
-const addReview = (user_id, place_id, comment, positive, cb) => {
-    let review_date = new Date();
-    let dd = review_date.getDay();
-    let mm = review_date.getMonth();
-    let year = review_date.getYear();
-    review_date = year+'-'+mm+'-'+dd;
-    console.log(review_date);
-    databaseConnection.query(
-      'INSERT INTO reviews (user_id, place_id, comment, positive, review_date) VALUES ($1, $2, $3, $4, $5);',
-      [user_id, place_id, comment, positive, review_date],
-      (err, res) => {
-        if (err) {
-          return cb(err);
-        } else {
-          cb(null, res);
-        }
-      });
-  };
+const addReview = (comment, positive, cb) => {
+  const now = new Date();
+  const reviewDate = `${now.getFullYear()}-${now.getMonth() +
+    1}-${now.getDate()}`;
+  console.log('reviewDate', reviewDate);
+  databaseConnection.query(
+    'INSERT INTO reviews (comment, positive, review_date) VALUES ($1, $2, $3);',
+    [comment, positive, reviewDate],
+    (err, res) => {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, res);
+    },
+  );
+};
 
 module.exports = { addPlace, addReview };
-
